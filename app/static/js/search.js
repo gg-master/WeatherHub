35be716ed0932909e7cbf94851276ebc9acc17c4
brookite <brookit18@gmail.com>
2023-11-20 11:57:14 +0300

    Added place search to frontend

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -1,38 +1,60 @@
-const searchInput = document.querySelector('#inputCity');
-const searchOptions = document.querySelector('.options');
+const searchInput = document.querySelector("#inputCity");
+const searchOptions = document.querySelector(".options");
 
 function getOptions(word) {
-    const lang = "ru_RU"; // TODO изменить жесткое задание локализации
-    const url = `/api/findLocation?lang=${lang}&query=${encodeURIComponent(word)}`;
+  const lang = "ru_RU"; // TODO изменить жесткое задание локализации
+  const url = `/api/findLocation?lang=${lang}&query=${encodeURIComponent(
+    word
+  )}`;
 
-    return fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Запрос завершился с ошибкой: ${response.status}`);
-            }
-            return response.json();
-        })
-        .catch(error => {
-            console.error(`Произошла ошибка при отправке запроса: ${error.message}`);
-        });
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Запрос завершился с ошибкой: ${response.status}`);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      console.error(`Произошла ошибка при отправке запроса: ${error.message}`);
+    });
+}
+
+function delay(fn, ms) {
+  let timer = 0;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(fn.bind(this, ...args), ms || 0);
+  };
 }
 
 function displayOptions() {
-    getOptions(this.value)
-        .then(options => {
-            options = options.result;
-            const html = options.map(city => {
-                return `<li><span>${city.name}</span></li>`;
-            })
-                .slice(0, 10)
-                .join('');
-
-            searchOptions.innerHTML = this.value ? html : null;
+  if (this.value.replace(/^\s+|\s+$/g, "") == "") {
+    searchOptions.innerHTML = "";
+    return;
+  }
+  getOptions(this.value)
+    .then((options) => {
+      options = options.result;
+      const html = options
+        .map((city) => {
+          return `<li class="placeOption"><span>${city.name}</span></li>`;
         })
-        .catch(error => {
-            console.error(`Произошла ошибка: ${error.message}`);
-        });
+        .slice(0, 10)
+        .join("");
+
+      searchOptions.innerHTML = this.value ? html : null;
+      Array.from(document.querySelectorAll(".placeOption")).map((el) => {
+        el.addEventListener("click", selectPlace);
+      });
+    })
+    .catch((error) => {
+      console.error(`Произошла ошибка: ${error.message}`);
+    });
 }
 
-searchInput.addEventListener('change', displayOptions);
-searchInput.addEventListener('keyup', displayOptions);
+searchInput.addEventListener("keyup", delay(displayOptions, 500));
+
+function selectPlace(event) {
+  let element = event.target;
+  window.location.href = `/forecast?name=${element.innerText}`;
+}
